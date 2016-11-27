264a919c18e226013aae61dd2b4780c9026c62ac
George <gtzintzarov@gmail.com>
2016-11-27 00:32:08 -0500

    gpo set testing

--- asyncRead.java
+++ asyncRead.java
@@ -133,7 +133,7 @@ public class asyncRead
 
 
             r.paramSet(TMConstants.TMR_PARAM_GPIO_OUTPUTLIST, new int[] {1,2} ); // I think the move is to put this before everything
-            new boolean[] lightArray = {true,false};
+            boolean[] lightArray = {true,false};
             for(int iii=1; iii < 3; iii++)
             {
                 try
@@ -167,7 +167,7 @@ public class asyncRead
 	        // END GPI 1
 	        
 	        r.paramSet(TMConstants.TMR_PARAM_GPIO_OUTPUTLIST, new int[] {1,2} ); // I think the move is to put this before everything
-            new boolean[] lightArray = {false,true};
+            lightArray = new boolean[] {false,true};
             for(int iii=1; iii < 3; iii++)
             {
                 try
@@ -231,7 +231,7 @@ public class asyncRead
 			
 			//r.reboot();
 			r.paramSet(TMConstants.TMR_PARAM_GPIO_OUTPUTLIST, new int[] {1,2} ); // I think the move is to put this before everything
-            new boolean[] lightArray = {true,false};
+            lightArray = new boolean[] {false,false};
             for(int iii=1; iii < 3; iii++)
             {
                 try
